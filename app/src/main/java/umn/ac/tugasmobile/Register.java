b2a0ac3abfe51f6ce80f9bd5329a0da0b0175c4a
ladytheresa <ladytheresa65@gmail.com>
2020-05-06 19:01:18 +0700

    register

--- app/src/main/java/umn/ac/tugasmobile/Register.java
+++ app/src/main/java/umn/ac/tugasmobile/Register.java
@@ -1,25 +1,39 @@
 package umn.ac.tugasmobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class Register extends AppCompatActivity {
 
-    Button cancel;
+    private Button cancel, register;
+    private EditText email, password;
+    private FirebaseAuth auth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
-
-
+        auth = FirebaseAuth.getInstance();
+        email =  findViewById(R.id.etEmail);
+        password = findViewById(R.id.etPassword);
+        register = findViewById(R.id.btnRegister);
         cancel = findViewById(R.id.btnCancel);
 
         cancel.setOnClickListener(new View.OnClickListener() {
@@ -31,7 +45,58 @@ public class Register extends AppCompatActivity {
             }
         });
 
-    }
 
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = ((EditText) findViewById(R.id.etEmail)).getText().toString();
+                String password = ((EditText) findViewById(R.id.etPassword)).getText().toString();
+
+                //String email = email.getText().toString().trim();
+                //String password = password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //create user
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(Register.this, "User successfully registered! You can now log in!" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(Register.this, "Register fail!" + task.getException(),
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    startActivity(new Intent(Register.this, MainActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
+
+            }
+        });
+
+
+    } //oncreate
 
 }
+
+
+
+
