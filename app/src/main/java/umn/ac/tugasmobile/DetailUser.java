0d196e8adf5c0f27477f882549790f9755a827cf
ladytheresa <ladytheresa65@gmail.com>
2020-05-10 19:34:46 +0700

    Added user detail, upload profile pic, update user profile

---
+++ app/src/main/java/umn/ac/tugasmobile/DetailUser.java
@@ -0,0 +1,66 @@
+package umn.ac.tugasmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class DetailUser extends AppCompatActivity {
+
+    TextView namalengkap, tvdisplay, tvangkatan, tvnim, tvstatus, tvpekerjaan,tvnohp,tvdomisili;
+    ImageView ivpp, back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_user);
+        final Intent i=getIntent();
+        namalengkap = findViewById(R.id.fullname);
+        tvdisplay = findViewById(R.id.username);
+        tvangkatan = findViewById(R.id.angkatan);
+        ivpp = findViewById(R.id.pp);
+        tvnim = findViewById(R.id.nim);
+        tvstatus = findViewById(R.id.Status);
+        tvpekerjaan = findViewById(R.id.Pekerjaan);
+        tvnohp = findViewById(R.id.nohp);
+        tvdomisili = findViewById(R.id.domisili);
+        back = findViewById(R.id.back);
+
+        String nama=i.getStringExtra("nama");
+        String displayname=i.getStringExtra("displayname");
+        String angkatan=i.getStringExtra("angkatan");
+        String pp=i.getStringExtra("pp");
+        String nim=i.getStringExtra("nim");
+        String status=i.getStringExtra("status");
+        String pekerjaan=i.getStringExtra("pekerjaan");
+        String nohp=i.getStringExtra("nohp");
+        String domisili=i.getStringExtra("domisili");
+
+        tvdisplay.setText(displayname);
+        namalengkap.setText(nama);
+        tvangkatan.setText(angkatan);
+        tvnim.setText(nim);
+        tvstatus.setText(status);
+        tvpekerjaan.setText(pekerjaan);
+        tvnohp.setText(nohp);
+        tvdomisili.setText(domisili);
+
+        if(pp.equals("default")){
+            ivpp.setImageResource(R.drawable.blank);
+        }
+        else{
+            Glide.with(DetailUser.this).load(pp).into(ivpp);
+        }
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+}
--- app/src/main/java/umn/ac/tugasmobile/UserAdapter.java
+++ app/src/main/java/umn/ac/tugasmobile/UserAdapter.java
@@ -2,11 +2,14 @@ package umn.ac.tugasmobile;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -33,6 +36,13 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         User user = mUsers.get(position);
         holder.tvDisplayName.setText(user.getDisplayName());
+        if(user.getProfilePic().equals("default")){
+            holder.ivPicture.setImageResource(R.drawable.blank);
+        }
+        else{
+            Glide.with(mContext).load(user.getProfilePic()).into(holder.ivPicture);
+        }
+
     }
 
     @Override
@@ -43,11 +53,31 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder{
         ImageView ivPicture;
         TextView tvDisplayName;
+        ImageView heartFavorite;
 
         public ViewHolder(@NonNull View itemView){
             super(itemView);
             ivPicture = itemView.findViewById(R.id.ivPicture);
             tvDisplayName = itemView.findViewById(R.id.tvDisplayName);
+            heartFavorite = itemView.findViewById(R.id.heartFavorite);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    User user=mUsers.get(getAdapterPosition());
+                    Intent i=new Intent(mContext, DetailUser.class);
+                    i.putExtra("angkatan",user.getAngkatan());
+                    i.putExtra("displayname",user.getDisplayName());
+                    i.putExtra("nama",user.getNamalengkap());
+                    i.putExtra("pp",user.getProfilePic());
+                    i.putExtra("nim",user.getNim());
+                    i.putExtra("status",user.getStatusPekerjaan());
+                    i.putExtra("pekerjaan",user.getPekerjaan());
+                    i.putExtra("nohp",user.getNomorhp());
+                    i.putExtra("domisili",user.getDomisili());
+                    mContext.startActivity(i);
+                }
+            });
         }
     }
 
